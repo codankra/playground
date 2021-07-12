c37d8663c43c877d8dbfac8ee1f3df8e8fd8fbe7
Daniel Kramer <codankra@gmail.com>
2021-07-12 17:07:49 -0400

    Add JS docs for typing and intellisense

--- algo.js
+++ algo.js
@@ -1,6 +1,12 @@
 // Generate list of lowercase characters in the English alphabet
 const alphabet = [...Array(26).keys()].map((i) => String.fromCharCode(i + 97));
 
+/**
+ * Shifts a character by a given magnitude.
+ * @param {string} char Character string of size 1
+ * @param {number} shiftSize The amount to shift
+ * @returns {string} a shifted character
+ */
 const getShiftedChar = (char, shiftSize) => {
   if (alphabet.includes(char)) {
     let shiftedCharIndex =
@@ -11,14 +17,26 @@ const getShiftedChar = (char, shiftSize) => {
   return char;
 };
 
+/**
+ * Rotates given text forward by the given shift size.
+ * @param {string} text
+ * @param {number} shiftSize
+ * @returns {string} The rotated text
+ */
 const rotateText = (text, shiftSize) => {
   return [...text].map((char) => getShiftedChar(char, shiftSize)).join("");
 };
 
+/**
+ * ROT[X] function for an alphabet. Rotates/shifts text.
+ * @param {string} text Text input from user or program
+ * @returns {Map<number, string>} The list of ciphers accross an alphabet
+ */
 const rotateTextThroughAlphabet = (text) => {
-  const rotatedTextMap = {};
+  /** @type {Map<number, string>} */
+  let rotatedTextMap;
   for (let i = 0; i < 26; i += 1) {
-    rotatedTextMap[i] = rotateText(text, i);
+    rotatedTextMap.set(i, rotateText(text, i));
   }
   return rotatedTextMap;
 };
